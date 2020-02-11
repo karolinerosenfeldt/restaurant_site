842a673c4cff0a72db65a1266e63df507f267b18
karoline rosenfeldt <karosenfeldt@gmail.com>
2020-02-11 10:32:58 +0100

    making categories

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
+fetch("https://kea-alt-del.dk/t5/api/categories")
+    .then(res=>res.json())
+    .then(createCategories)
+
+function createCategories(data){
+    console.log(data)
+    data.forEach(function(oneCat){
+
+        const section = document.createElement("section");
+        section.id = oneCat;
+        const h2 = document.createElement("h2");
+        h2.textContent = oneCat;
+        section.appendChild(h2);
+
+        document.querySelector("#wrapper").appendChild(section);
+    })
+    getProducts();
+}
+
+
+function getProducts(){
 fetch("https://kea-alt-del.dk/t5/api/productlist")
-.then(function(response){
-    return response.json()
-})
+    .then(function (response) {
+        return response.json()
+    })
 
-.then(function (data){
-    const superheroes = data
-    data.forEach(showDish)
-})
+    .then(function (data) {
+        const superheroes = data
+        data.forEach(showDish)
+    })
+}
 
-function showDish(course){
+function showDish(course) {
     console.log(course)
     const template = document.querySelector("template").content;
     const clone = template.cloneNode(true);
@@ -19,24 +41,21 @@ function showDish(course){
 
     clone.querySelector(".info").textContent = course.shortdescription;
 
-    clone.querySelector(".price-discount").textContent = course.discount;
+    clone.querySelector(".price-discount span").textContent = course.discount;
 
-    if (course.discount){
+    if (course.discount) {
         clone.querySelector(".price-discount").textContent = course.price;
         const newPrice = Math.round(course.price - course.price * course.discount / 100);
 
         clone.querySelector(".price-full span").textContent = newPrice;
-    } else{
+    } else {
         clone.querySelector(".price-discount").remove()
         clone.querySelector(".price-full span").textContent = course.price
     }
 
-    const parent = document.querySelector("#starters");
-    parent.appendChild(clone);
+    console.log(`#${course.category}`)
+    document.querySelector(`#${course.category}`).appendChild(clone);
+
+    /*const parent = document.querySelector("#starter");
+    parent.appendChild(clone);*/
 }
-
-
-
-
-
-
